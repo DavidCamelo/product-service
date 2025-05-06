75465d3e32675c500f328961f68e08ca2bd98bbb
David Camelo <dacaro16@gmail.com>
2025-05-06 14:07:36 -0500

    Setup Product Service

--- src/main/java/com/davidcamelo/product/dto/UserDTO.java
+++ src/main/java/com/davidcamelo/product/dto/UserDTO.java
@@ -8,7 +8,10 @@ import lombok.Builder;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record UserDTO(
         Long id,
+        @JsonProperty(access = JsonProperty.Access.READ_ONLY)
         String name,
+        @JsonProperty(access = JsonProperty.Access.READ_ONLY)
         String lastName,
+        @JsonProperty(access = JsonProperty.Access.READ_ONLY)
         ErrorDTO error
 ) { }
