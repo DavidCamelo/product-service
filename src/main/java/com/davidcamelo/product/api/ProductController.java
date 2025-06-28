f4e2226b651f0ed3c9b6c5d621909e52fc630900
David Camelo <dacaro16@gmail.com>
2025-06-27 21:50:41 -0500

    Setup Product Service

--- src/main/java/com/davidcamelo/product/api/ProductController.java
+++ src/main/java/com/davidcamelo/product/api/ProductController.java
@@ -15,10 +15,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Product API")
 @RestController
+@RequestMapping("/api/products")
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
