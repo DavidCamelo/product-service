23a47f0ed4a2a88ba2bab6d50e50ebfbc49e310e
David Camelo <dacaro16@gmail.com>
2025-12-03 16:07:16 -0500

    Upgrade to Spring Boot 4

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## Product Service - [Open API](https://spring-boot.davidcamelo.com/swagger-ui/index.html)
-[![Infra Deployment](https://github.com/DavidCamelo/product-service/actions/workflows/deploy.yml/badge.svg)](https://github.com/DavidCamelo/product-service/actions/workflows/deploy.yml)
-
+[![Docker Image CI / Kubernetes Deployment CD](https://github.com/DavidCamelo/product-service/actions/workflows/deploy.yml/badge.svg)](https://github.com/DavidCamelo/product-service/actions/workflows/deploy.yml)
+### Overview
 This project is based on Spring Boot and manages product-related operations within the application ecosystem. It provides RESTful APIs for product catalog management, including product creation, retrieval, updating, and deletion functionalities.
--- src/main/java/com/davidcamelo/product/service/impl/UserServiceImpl.java
+++ src/main/java/com/davidcamelo/product/service/impl/UserServiceImpl.java
@@ -1,29 +1,22 @@
 package com.davidcamelo.product.service.impl;
 
 import com.davidcamelo.product.dto.FilterDTO;
-import com.davidcamelo.product.dto.RestClientResponse;
 import com.davidcamelo.product.dto.UserDTO;
-import com.davidcamelo.product.error.ProductException;
 import com.davidcamelo.product.service.UserService;
-import com.davidcamelo.product.util.RestClientUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
-    private final RestClientUtil<UserDTO> restClientUtil;
     private static final String USER_SERVICE = "users";
 
     @Override
     public UserDTO create(UserDTO userDTO) {
-        return handleResponse(restClientUtil.create(USER_SERVICE, userDTO, UserDTO.class));
+        return userDTO;
     }
 
     @Override
@@ -31,19 +24,11 @@ public class UserServiceImpl implements UserService {
         if (id == null) {
             return null;
         }
-        return handleResponse(restClientUtil.getById(USER_SERVICE, id, UserDTO.class));
+        return UserDTO.builder().build();
     }
 
     @Override
     public Page<UserDTO> getAll(FilterDTO filterDTO) {
-        var response = restClientUtil.getAll(USER_SERVICE, filterDTO, Page.class);
-        if (response.getDTO() != null) {
-            return response.getDTO();
-        }
-        if (response.getErrorDTO() != null) {
-            var errorList = List.of(UserDTO.builder().error(response.getErrorDTO()).build());
-            return new PageImpl<>(errorList);
-        }
         return null;
     }
 
@@ -52,7 +37,7 @@ public class UserServiceImpl implements UserService {
         if (id == null) {
             return null;
         }
-        return handleResponse(restClientUtil.update(USER_SERVICE, id, userDTO, UserDTO.class));
+        return userDTO;
     }
 
     @Override
@@ -60,19 +45,5 @@ public class UserServiceImpl implements UserService {
         if (id == null) {
             return;
         }
-        var response = restClientUtil.delete(USER_SERVICE, id);
-        if (response.getErrorDTO() != null) {
-            throw new ProductException(response.getErrorDTO());
-        }
-    }
-
-    private UserDTO handleResponse(RestClientResponse<UserDTO> userResponse) {
-        if (userResponse.getDTO() != null) {
-            return userResponse.getDTO();
-        }
-        if (userResponse.getErrorDTO() != null) {
-            return UserDTO.builder().error(userResponse.getErrorDTO()).build();
-        }
-        return null;
     }
 }
